f1dabaef89125f6fd42d813d2c1ecce558d84ad3
Pengchenmeng <2424234410@qq.com>
2017-10-27 10:22:02 +0800

    xg

---
+++ app/js/regex.js
@@ -0,0 +1,54 @@
+// 使用u修饰符后，所有量词都会正确识别码点大于0xFFFF的 Unicode 字符（识别四个字节的 UTF-16 编码为一个字符）。
+  // 1、点字符（.），在正则表达式中，含义是除了换行符以外的任意单个字符。对于码点大于0xFFFF的 Unicode 字符，点字符不能识别，必须加上u修饰符
+    // var s = '𠮷';
+    // console.log(/^.$/.test(s));//false
+    // console.log(/^.$/u.test(s));//true
+  // 2、Unicode 字符表示法（大括号表示），在正则表达式中必须加上u修饰符，才能识别当中的大括号，否则会被解读为量词
+    // console.log(/\u{61}/.test('a')); //false
+    // console.log(/\u{61}/u.test('a')); // true
+  // 3、量词
+    // console.log(/u{3}/.test('uuu')); //true 匹配三个连续的u
+    /*console.log(/a{2}/.test('aa')) // true
+    console.log(/a{2}/u.test('aa')) // true
+    console.log(/𠮷{2}/.test('𠮷𠮷')) // false
+    console.log(/𠮷{2}/u.test('𠮷𠮷')) // true*/
+  // 4、预定义模式--\S是预定义模式，匹配所有不是空格的字符
+    // console.log(/^\S$/.test('𠮷')); // false
+    // console.log(/^\S$/u.test('𠮷')); // true
+    // function codePointLength(text) {
+    //   var result = text.match(/[\s\S]/gu);
+    //   return result ? result.length : 0;
+    // }
+    // function clickme9(){
+    //   var s = '𠮷𠮷';
+    //   console.log('"'+s+'"字符串通过原生的方法返回的长度为：'+s.length); // 4
+    //   console.log('"'+s+'"字符串通过识别Unicode码的方法返回正确的长度为：'+codePointLength(s)); // 2
+    // }
+// y修饰符，粘连”（sticky）修饰符，全局匹配，后一次匹配都从上一次匹配成功的下一个位置开始，
+//         g修饰符只要剩余位置中存在匹配就可，而y修饰符确保匹配必须从剩余的第一个位置开始
+  // var s = 'aaa_aa_a';
+  // var r1 = /a+/g;
+  // var r2 = /a+/y;
+  // console.log(r1.exec(s));//["aaa", index: 0, input: "aaa_aa_a"]
+  // console.log(r2.exec(s));//["aaa", index: 0, input: "aaa_aa_a"]
+  // console.log(r1.exec(s));//["aa", index: 4, input: "aaa_aa_a"]---g修饰没有位置要求，所以第二次执行会返回结果
+  // console.log(r2.exec(s));//null---y修饰符要求匹配必须从头部开始，所以返回null
+// 改一下正则表达式，保证每次都能头部匹配，y修饰符就会返回结果了
+  // var s = 'aaa_aa_a';
+  // var r = /a+_/y; 
+  // r.exec(s) // ["aaa_"]
+  // r.exec(s) // ["aa_"]
+// sticky属性--与y修饰符匹配，表示是否设置了y修饰符
+  // var r = /hello\d/y;
+  // console.log(r.sticky);//true
+// flags属性--返回正则表达式的修饰符
+  // ES5 的 source 属性
+  // 返回正则表达式的正文
+  console.log(/abc/ig.source);
+  // "abc"
+
+  // ES6 的 flags 属性
+  // 返回正则表达式的修饰符
+  console.log(/abc/ig.flags)
+  // 'gi'
+// s修饰符：dotAll模式
--- app/js/stringEx.js
+++ app/js/stringEx.js
@@ -1,10 +1,25 @@
+// 4.字符串的遍历器接口
+    // for (let codePoint of 'foo') {
+    //   console.log(codePoint)
+    // }
+    // let text = String.fromCodePoint(0x20BB7);
+
+    // for (let i = 0; i < text.length; i++) {    //传统的for循环不能识别大于0xFFFF的码点
+    //   console.log(text[i]);
+    // }
+    // // " "
+    // // " "
+
+    // for (let i of text) {         //此循环能够识别
+    //   console.log(i);
+    // }
 // 7、includes()：返回布尔值，表示是否找到了参数字符串。
     // startsWith()：返回布尔值，表示参数字符串是否在原字符串的头部。
     // endsWith()：返回布尔值，表示参数字符串是否在原字符串的尾部。
     // 使用第二个参数n时，endsWith它针对前n个字符，而其他两个方法针对从第n个位置直到字符串结束。
     // let s = 'hello world!';
     // console.log(s.startsWith('w',7));
-    // console.log(s.endsWith('w',5));
+    // console.log(s.endsWith('o',5));
     // console.log(s.includes('o'));
 // 8、repeat()返回一个新字符串，表示将原字符串重复n次
     // console.log('x'.repeat(3));
@@ -18,19 +33,40 @@
     // console.log('na'.repeat('na'));//' '如果repeat的参数是字符串，则会先转换成数字。
     // console.log('na'.repeat(3));//nanana
 // 9、字符串补全长度：padStart()用于头部补全，padEnd()用于尾部补全
+    // console.log('12'.padStart(10, 'YYYY-MM-DD')); // "YYYY-MM-12"
+    // console.log('09-12'.padStart(10, 'YYYY-MM-DD')); // "YYYY-09-12"
 // 10、模版字符串
     // let name = 'sunny';
     // console.log(`hello ${name},string text line 1
     // string text line 2`);
-    console.log(`
-    <ul>
-        <li>first</li>
-        <li>second</li>
-    </ul>
-    `);
-    console.log(`
-    <ul>
-        <li>first</li>
-        <li>second</li>
-    </ul>
-    `.trim());
+    // console.log(`
+    // <ul>
+    //     <li>first</li>
+    //     <li>second</li>
+    // </ul>
+    // `);
+    // console.log(`
+    // <ul>
+    //     <li>first</li>
+    //     <li>second</li>
+    // </ul>
+    // `.trim());
+// 如果需要引用模板字符串本身，在需要时执行，可以像下面这样写。
+// 写法一
+// let str1 = 'return ' + '`Hello ${name}!`';
+// let func1 = new Function('name', str1);
+// console.log(func1('Jack')); // "Hello Jack!"
+
+// // 写法二
+// let str2 = '(name) => `Hello ${name}!`';
+// let func2 = eval.call(null, str2);
+// console.log(func2('Jack')); // "Hello Jack!"
+// 模板处理函数的第一个参数（模板字符串数组），还有一个raw属性。
+// tag`First line\nSecond line`
+
+// function tag(strings) {
+//     console.log(strings);
+//     console.log(strings.raw[0]);
+//   // strings.raw[0] 为 "First line\\nSecond line"
+//   // 打印输出 "First line\nSecond line"
+// }
