8a747405160b0256e0b91f8cbe62189eded34de0
Baohairen <572525186@qq.com>
2017-10-31 09:39:45 +0800

    tmp

---
+++ app/dist/js/arr.js
@@ -0,0 +1,1 @@
+"use strict";function arr(){console.log([1,2,3].includes(2)),console.log([1,2,3].includes(4)),console.log([1,2,NaN].includes(NaN))}var add=function(n,o){return n+o};
---
+++ app/dist/js/es6.js
@@ -0,0 +1,1 @@
+"use strict";function clickme1(){for(var c=[],o=0;o<10;o++)!function(o){c[o]=function(){$("#result1").val(o)},c[0]()}(o);c[6]()}function clickme2(){for(c=0;c<3;c++){var c="abc";console.log(c)}}function clickme3(){console.log(c);var c=2;console.log(o);var o=3}function clickme4(){}function clickme5(){}
